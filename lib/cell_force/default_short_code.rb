7c78fac8d8f5e2acea4d79fc90585595fc1ae117
Isaac Betesh <iybetesh@gmail.com>
2015-03-30 15:35:50 -0400

    moved method for identifying default short code into a separate module

---
+++ lib/cell_force/default_short_code.rb
@@ -0,0 +1,17 @@
+require "cell_force/api"
+
+module CellForce
+  module DefaultShortCode
+    def short_code_id
+      @short_code_id ||= begin
+        data = Api.post("shortcode/getusershortcodes").data
+        if short_code
+          data.find{ |e| short_code.to_s == e["shortcode"].to_s }
+        else
+          # If the instance was not initialized with a short_code, we use the first one we find
+          data[0].tap { |s| @short_code = s["shortcode"] }
+        end["shortcode_id"]
+      end
+    end
+  end
+end
--- lib/cell_force/mobile_device.rb
+++ lib/cell_force/mobile_device.rb
@@ -1,7 +1,9 @@
-require "cell_force/api"
+require "cell_force/default_short_code"
 
 module CellForce
   class MobileDevice
+    include DefaultShortCode
+
     attr_reader :phone, :short_code
     def initialize(phone, short_code=nil)
       @phone, @short_code = phone, short_code
@@ -9,19 +11,6 @@ module CellForce
 
     def send_mt(message)
       Api.post(Api::SEND_SMS_RESOURCE, sms_validation: SmsValidation::Sms.new(phone, message), shortcode_id: short_code_id)
-    end
-
-    private
-    def short_code_id
-      @short_code_id ||= begin
-        data = Api.post("shortcode/getusershortcodes").data
-        if short_code
-          data.find{ |e| short_code.to_s == e["shortcode"].to_s }
-        else
-          # If the instance was not initialized with a short_code, we use the first one we find
-          data[0].tap { |s| @short_code = s["shortcode"] }
-        end["shortcode_id"]
-      end
     end
   end
 end
